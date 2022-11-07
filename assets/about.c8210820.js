54c99160cce1240043e176fa65e77c4e967b4772
Renovamen <Renovamen@users.noreply.github.com>
2022-11-07 21:14:20 +0000

    deploy: b6bd3d0c8e448e51c36ae3f2d93ac5e37dd56a7a

--- assets/about.c8210820.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as n,c,w as a,a as s,r as _,b as r}from"./app.0c63d97d.js";const u={};function p(f,i){const o=_("RouterView"),e=r;return n(),c(e,{class:"about"},{default:a(()=>[s(o)]),_:1})}const m=t(u,[["render",p]]);export{m as default};
