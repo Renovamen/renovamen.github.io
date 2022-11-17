dbc116994e9f06ec95fb61279f85c1b19a97c9f9
Renovamen <Renovamen@users.noreply.github.com>
2022-11-17 00:44:27 +0000

    deploy: 9331d5e81f2d3e72fb62f95c72aabb7c465be796

---
+++ assets/about.aaf09eb2.js
@@ -0,0 +1,1 @@
+import{_ as t,o as n,c,w as a,a as s,r as _,b as r}from"./app.69e5511d.js";const u={};function p(f,i){const o=_("RouterView"),e=r;return n(),c(e,{class:"about"},{default:a(()=>[s(o)]),_:1})}const m=t(u,[["render",p]]);export{m as default};
