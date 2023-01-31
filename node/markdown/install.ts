ccfa0953546a57807813d8a17b90f14bbaf3d5ab
邹笑寒 <renovamenzxh@gmail.com>
2023-02-01 00:10:59 +0800

    fix: ensure anchors don't start with a number

--- node/markdown/install.ts
+++ node/markdown/install.ts
@@ -1,5 +1,4 @@
 import type MarkdownIt from "markdown-it";
-import { slugify } from "@renovamen/utils";
 import linkAttributesPlugin from "markdown-it-link-attributes";
 // @ts-expect-error missing types
 import tocPlugin from "markdown-it-table-of-contents";
@@ -9,13 +8,36 @@ import codeBlockPlugin from "./codeBlock";
 import highlightLinesPlugin from "./highlightLines";
 import containerPlugin from "./container";
 
+// https://github.com/mdit-vue/mdit-vue/blob/main/packages/shared/src/slugify.ts
+const rControl = /[\u0000-\u001f]/g;
+const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’<>,.?/]+/g;
+const rCombining = /[\u0300-\u036F]/g;
+
+const slugify = (str: string): string =>
+  str
+    .normalize("NFKD")
+    // Remove accents
+    .replace(rCombining, "")
+    // Remove control characters
+    .replace(rControl, "")
+    // Replace special characters
+    .replace(rSpecial, "-")
+    // Remove continuos separators
+    .replace(/-{2,}/g, "-")
+    // Remove prefixing and trailing separators
+    .replace(/^-+|-+$/g, "")
+    // ensure it doesn't start with a number
+    .replace(/^(\d)/, "_$1")
+    // lowercase
+    .toLowerCase();
+
 export const installMarkdownPlugins = (md: MarkdownIt) => {
   md.use(highlightLinesPlugin);
   md.use(codeBlockPlugin);
   md.use(containerPlugin);
 
   md.use(anchorPlugin, {
-    slugify: (s) => decodeURI(slugify(s)),
+    slugify,
     permalink: anchorPlugin.permalink.linkInsideHeader({
       symbol: "#",
       ariaHidden: true
