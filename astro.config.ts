b280a79e9aacc7cee909857c2f35ba8a8922d1da
邹笑寒 <renovamenzxh@gmail.com>
2023-04-29 18:25:37 +0800

    chore: bump deps

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,7 @@ import { remarkPlugins, rehypePlugins } from "./plugins";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zxh.io/",
-  integrations: [solid(), unocss(), sitemap()],
+  integrations: [solid(), unocss({ injectReset: true }), sitemap()],
   markdown: {
     remarkPlugins,
     rehypePlugins,
