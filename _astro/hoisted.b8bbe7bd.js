02fbb6eff06bc9db8f05d574ddc8b765e0a4613f
Renovamen <Renovamen@users.noreply.github.com>
2023-05-24 11:39:42 +0000

    deploy: 61c1e2c030143945c94d4aef062bd5ea8dd51d1f

---
+++ _astro/hoisted.b8bbe7bd.js
@@ -0,0 +1,1 @@
+import"./hoisted.d750298c.js";import{m as o}from"./medium-zoom.esm.e8eb64a8.js";const m=o();m.attach(":not(a) > img:not(.no-zoom)");
--- _astro/hoisted.f3d5dd5e.js
+++
@@ -1,1 +0,0 @@
-import"./Layout.astro_astro_type_script_index_0_lang.d750298c.js";
