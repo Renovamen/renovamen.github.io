5af712667ec8b9f71be1f0252690df613bee8554
邹笑寒 <renovamenzxh@gmail.com>
2024-04-09 04:29:56 -0400

    chore: bump deps

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-const astro = require("@renovamen/eslint-config-astro");
-const solid = require("@renovamen/eslint-config-solid");
-
-module.exports = {
-  extends: ["@renovamen/eslint-config-astro", "@renovamen/eslint-config-solid"],
-  overrides: astro.overrides.concat(solid.overrides),
-  ignorePatterns: ["node_modules/", "dist/", "auto-imports.d.ts"]
-};
