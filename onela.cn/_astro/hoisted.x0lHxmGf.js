7142fb66796aeb3876e75ffd6d5eecabef7c0be4
wei.zou <zouwei@kaadas.com>
2024-06-06 09:24:47 +0800

    fix:add doc

---
+++ onela.cn/_astro/hoisted.x0lHxmGf.js
@@ -0,0 +1,1 @@
+import"./hoisted.ChQ-AT7g.js";const o=["\\(o_o)/","(o^^)o","(˚Δ˚)b","(^-^*)","(^_^)b","(╯‵□′)╯","(='X'=)","(>_<)","\\(°ˊДˋ°)/","ㄟ(▔▽▔)ㄏ"],e=document.querySelector("#emoji");e&&(e.innerHTML=o[Math.floor(Math.random()*o.length)]);
